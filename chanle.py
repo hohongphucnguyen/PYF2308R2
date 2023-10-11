7f5a4ec9d2975393c7235777a17b73c9d22f0f3f
hohongphucnguyen <hohongphucnguyen@gmail.com>
2023-10-11 15:38:17 +0700

    bai tap

---
+++ chanle.py
@@ -0,0 +1,7 @@
+so = float(input(' nhấp vào số cần kiểm tra '))
+if so % 2 == 0:
+    print(f' {so} là số chẵn ')
+if so % 2 == 1:
+    print(f' {so} là số lẻ ')
+else:
+    print(f' {so} ko phải là số tự nhiên ')
